1d6dc5406ded63c386a93f65296ebe7a9d9d5e74
alexsemenov-java <asemenov.java@gmail.com>
2020-12-06 13:40:34 +0300

    Added Menu for users.

--- src/src/DevGame.java
+++ src/src/DevGame.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
+
 public class DevGame {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String command;
+
+        do {
+
+            System.out.println("Welcome to JavaGame #Collector100. Please make your choice and press Enter:  ");
+            System.out.println("1. Start new game ");
+            System.out.println("2. Options ");
+            System.out.println("3. Exit ");
+
+            command = scanner.nextLine();
+
+            switch (command){
+
+            }
+
+
+        }
+        while (!command.equals("4"));
 
 
     }
